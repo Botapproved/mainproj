4bda9695c3d463fa43e9a4fdcca61c1ba8739c62
Botapproved <samstephenthomas2024@cs.ajce.in>
2023-11-20 22:17:36 +0530

    report

--- project/components/Header/menuData.tsx
+++ project/components/Header/menuData.tsx
@@ -25,6 +25,12 @@ const menuData: Menu[] = [
     path: "/contact",
     newTab: false,
   },
+  {
+    id: 4,
+    title: "Forum",
+    path: "/forum",
+    newTab: false,
+  },
   // {
   //   id: 4,
   //   title: "Pages",
